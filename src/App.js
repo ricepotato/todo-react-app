81de2ac4462f5fcb6ae26a4b0e04cea33ba6392d
ricepotato <sj@mobills.co.kr>
2019-12-29 23:19:39 +0900

    Todo list function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import TodoTemplate from "./TodoTemplate";
+import TodoInsert from "./TodoInsert";
+import TodoList from "./TodoList";
 
 function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, text: "리액트 기초 알아보기", checked: true },
+    { id: 2, text: "컴포넌트 스타일링 해보기", checked: true },
+    { id: 3, text: "일정 관리 앱 만들어 보기", checked: false },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TodoTemplate>
+      <TodoInsert></TodoInsert>
+      <TodoList todos={todos}></TodoList>
+    </TodoTemplate>
   );
 }
 
